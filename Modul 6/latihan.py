4c3a4f18ec8e66b331c6b1c84044917e73fffe48
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2022-06-02 13:41:12 +0700

    modul 7 dan 8

--- Modul 6/latihan.py
+++ Modul 6/latihan.py
@@ -1,109 +1,119 @@
-# NO 1
+# Nomor 1
+import time
+from random import shuffle as kocok
+from time import time as detak
 
-##P = [2,8,15,23,37]
-##Q = [4,6,15,20]
-##R = gabungkanDuaListUrut(P, Q)
-# print(R)
 
-# def gabungkanDuaListUrut(A,B):
-##    la = len(A); lb = len(B)
-##    C = list()
-##    i = 0; j = 0
-##
-# while i < la and j < lb:
-# if A[1] < B[j]:
-# C.append(A[i])
-##            i += 1
-# else :
-# C.append(B[j])
-##            j += 1
-# while i < la:
-# C.append(A[i])
-##        i += 1
-# while j < lb:
-# C.append(B[j])
-##        j += 1
-# return C
+class MhsTIF(object):
+    def __init__(self, nama, nim, tinggal, us):
+        self.nama = nama
+        self.nim = nim
+        self.tinggal = tinggal
+        self.us = us
 
-# NO 2
-# eksekusi gui shell liat di laporan
-# def mergeShort(A):
-# if len(A) > 1:
-##        mid = len(A) // 2
-##        separuhKiri = A[:mid]
-##        separuhKanan = A[mid:]
-##
-# mergeShort(separuhKiri)
-# mergeShort(separuhKanan)
-##
-##        i=0 ; j=0 ; k=0
-# while i < len(separuhKiri) and j < len(separuhKanan):
-# if separuhKiri[i] < separuhKanan[j]:
-##                A[k] = separuhKiri[i]
-##                i = i + 1
-# else :
-##                A[k] = separuhKanan[j]
-##                j = j+1
-# k=k+1
-##
-# while i < len(separuhKiri):
-##            A[k] = separuhKiri[i]
-##            i = i+1
-##            k = k+1
-##
-# while j < len(separuhKanan):
-##            A[k] = separuhKanan[j]
-##            j = j+1
-##            k = k+1
-##
-# NO 3
-# def quickSort(A):
-#     quickSortBantu(A, 0, len(A) - 1)
 
-# def quickShortBantu(A, awal, akhir):
-#     if awal < akhir:
-#         titikBelah = partisi(A, awal, akhir)
-#         quickSortBantu(A, awal, titikBelah -1)
-#         quickSortBantu(A, titikBelah + 1, akhir)
+c0 = MhsTIF('Ika', 'L20019001', 'Sukoharjo', 240000)
+c1 = MhsTIF('Budi', 'L20019003', 'Sragen', 230000)
+c2 = MhsTIF('Ahmad', 'L20019002', 'Surakarta', 250000)
+c3 = MhsTIF('Chandra', 'L20019004', 'Surakarta', 235000)
+c4 = MhsTIF('Eka', 'L20019006', 'Boyolali', 240000)
+c5 = MhsTIF('Fandi', 'L20019005', 'Salatiga', 250000)
+c6 = MhsTIF('Deni', 'L20019007', 'Klaten', 245000)
+c7 = MhsTIF('Galuh', 'L20019009', 'Wonogiri', 245000)
+c8 = MhsTIF('Janto', 'L20019008', 'Klaten', 245000)
+c9 = MhsTIF('Hasan', 'L20019011', 'Karanganyar', 270000)
+c10 = MhsTIF('Khalid', 'L20019010', 'Purwodadi', 265000)
 
-# def partisi(A, awal, akhir):
-#     nilaiPivot = A[awal]
+Daftar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9]
 
-#     penandaKiri = awal + 1
-#     penandaKanan = akhir
 
-#     selesai = False
-#     while not selesai:
-#         while penandaKiri <= penandaKanan and\
-#               A[penandaKiri] <= nilaiPivot:
-#             penandaKiri = penandaKiri + 1
+class Node():
+    def __init__(self, data, tautan=None):
+        self.data = data
+        self.tautan = tautan
 
-#         while A[penandaKanan] >= nilaiPivot and\
-#               penandaKanan >= penandaKiri:
-#             penandaKanan = penandaKanan - 1
-#         if penandaKanan < penandaKiri:
-#             selesai = True
-#         else :
-#             temp = A[penandaKiri]
-#             A[penandaKiri] = A[penandaKanan]
-#             A[penandaKanan] = temp
 
-#     temp = A[awal]
-#     A[awal] = A[penandaKanan]
-#     A[penandaKanan] = temp
+def cetak(head):
+    curr = head
+    while curr is not None:
+        try:
+            print(curr.data)
+            curr = curr.tautan
+        except:
+            pass
 
-#     return penandaKanan
 
-from lat2 import MhsTIF
-c0 = MhsTIF('Ika', 10, 'Sukoharjo', 240000)
-c1 = MhsTIF('Budi', 51, 'Sragen', 230000)
-c2 = MhsTIF('Ahmad', 2, 'Surakarta', 250000)
-c3 = MhsTIF('Chandra', 18, 'Surakarta', 235000)
-c4 = MhsTIF('Eka', 4, 'Boyolali', 230000)
-c5 = MhsTIF('Fandi', 31, 'Salatiga', 250000)
-c6 = MhsTIF('Deni', 13, 'Klaten', 245000)
-c7 = MhsTIF('Galuh', 5, 'Wonogiri', 245000)
-c8 = MhsTIF('Janto', 23, 'Klaten', 245000)
-c9 = MhsTIF('Hasan', 64, 'Karanganyar', 270000)
-c10 = MhsTIF('Khalid', 29, 'Purwodadi', 265000)
-daftar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+a = Node(80)
+b = Node(7)
+c = Node(24)
+d = Node(16)
+e = Node(43)
+f = Node(91)
+g = Node(35)
+h = Node(2)
+i = Node(19)
+j = Node(72)
+
+a.tautan = b
+b.tautan = c
+c.tautan = d
+d.tautan = e
+e.tautan = f
+f.tautan = g
+g.tautan = h
+h.tautan = i
+i.tautan = j
+
+
+def mergeSortLinkedList(A):
+    linked = A
+    try:
+        daftar = []
+        curr = A
+        while curr:
+            daftar.append(curr.data)
+            curr = curr.tautan
+        A = daftar
+    except:
+        A = A
+
+    if len(A) > 1:
+        mid = len(A) // 2
+        separuhkiri = A[:mid]
+        separuhkanan = A[mid:]
+
+        mergeSortLinkedList(separuhkiri)
+        mergeSortLinkedList(separuhkanan)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(separuhkiri) and j < len(separuhkanan):
+            if separuhkiri[i] < separuhkanan[j]:
+                A[k] = separuhkiri[i]
+                i = i + 1
+            else:
+                A[k] = separuhkanan[j]
+                j = j + 1
+            k = k+1
+
+        while i < len(separuhkiri):
+            A[k] = separuhkiri[i]
+            i = i + 1
+            k = k+1
+
+        while j < len(separuhkanan):
+            A[k] = separuhkanan[j]
+            j = j + 1
+            k = k+1
+
+    for x in A:
+        try:
+            linked.data = x
+            linked = linked.tautan
+        except:
+            pass
+
+
+mergeSortLinkedList(a)
+cetak(a)
--- Modul 7/latihan.py
+++ Modul 7/latihan.py
@@ -1,44 +1,121 @@
-# ================Latihan================
+# # ================Latihan================
 import re
-s = 'Sebuah contoh kata:teh!!'
-cocok = re.findall(r'kata:\w\w\w', s)
+# s = 'Sebuah contoh kata:teh!!'
+# cocok = re.findall(r'kata:\w\w\w', s)
 
-if cocok:
-    print('menemukan', cocok)
-else:
-    print('tidak menemukan')
+# if cocok:
+#     print('menemukan', cocok)
+# else:
+#     print('tidak menemukan')
 
-# ================Latihan================
-s = 'Sebuah contoh kata:batagor!!'
-cocok = re.findall(r'kata:\w\w\w', s)
+# # ================Latihan================
+# s = 'Sebuah contoh kata:batagor!!'
+# cocok = re.findall(r'kata:\w\w\w', s)
 
-if cocok:
-    print('menemukan', cocok)
-else:
-    print('tidak menemukan')
+# if cocok:
+#     print('menemukan', cocok)
+# else:
+#     print('tidak menemukan')
 
-s = 'Sebuah contoh kata:es teh!!'
-cocok = re.findall(r'kata:\w\w\w', s)
+# s = 'Sebuah contoh kata:es teh!!'
+# cocok = re.findall(r'kata:\w\w\w', s)
 
-if cocok:
-    print('menemukan', cocok)
-else:
-    print('tidak menemukan')
+# if cocok:
+#     print('menemukan', cocok)
+# else:
+#     print('tidak menemukan')
 
-# ==============Latihan 2====================
-# Dua baris ini mencari pola ’eee’ distring ’teeeh’.
-# Seluruh pola harus cocok,tapi itu bisa muncul dimana saja.
-# Jika berhasil, \texttt{cocok} adalah daftar semua teks yang cocok.
-cocok1 = re.findall(r'eee', 'teeeh')  # => cocok == [’eee’]
-cocok2 = re.findall(r'ehs', 'teeeh')  # => cocok == []
+# # ==============Latihan 2====================
+# # Dua baris ini mencari pola ’eee’ distring ’teeeh’.
+# # Seluruh pola harus cocok,tapi itu bisa muncul dimana saja.
+# # Jika berhasil, \texttt{cocok} adalah daftar semua teks yang cocok.
+# cocok1 = re.findall(r'eee', 'teeeh')  # => cocok == [’eee’]
+# cocok2 = re.findall(r'ehs', 'teeeh')  # => cocok == []
 
-# .=semuakarakterkecuali\n
-cocok3 = re.findall(r'..h', 'teeeh')  # => cocok == ['eeh']
+# # .=semuakarakterkecuali\n
+# cocok3 = re.findall(r'..h', 'teeeh')  # => cocok == ['eeh']
 
-# \d = karakterangka,\w = karakter huruf atau angka
-# => cocok == [’123’,’201’]
-cocok4 = re.findall(r'\d\d\d', 't123h di 2019 bulan 02')
-# => cocok == [’def’,’tgh’]
-cocok5 = re.findall(r'\w\w\w', '@@a*bc#def*tghh!!')
+# # \d = karakterangka,\w = karakter huruf atau angka
+# # => cocok == [’123’,’201’]
+# cocok4 = re.findall(r'\d\d\d', 't123h di 2019 bulan 02')
+# # => cocok == [’def’,’tgh’]
+# cocok5 = re.findall(r'\w\w\w', '@@a*bc#def*tghh!!')
 
-print(cocok1, cocok2, cocok3, cocok4, cocok5)
+# print(cocok1, cocok2, cocok3, cocok4, cocok5)
+
+
+# #===================Latihan 3==========================#
+# # ==========LATIHAN 7.3.1==========
+# # e+ = satu atau lebih e,sebanyak-banyaknya.
+# cocok1 = re.findall(r'e+', 'ghdteeeh')  # => cocok == [’teee’]
+
+# # Menemukan solusi yang paling kiri,dan dari situ mendorong sitanda ‘+’
+# # sejauh-jauhnya(ingat’paling kiri dan paling besar’).
+# # Pada contoh ini,perhatikan bahwa pencarian menemukan dua pola yang tepat.
+# cocok2 = re.findall(r'e+', 'teeheeee')
+
+# # \s* = nol atau lebih karakter putih(spasi,tab,dsb.)
+# # Disini mencari 3 angka,kemungkinan dipisahkan oleh spasi / tab
+# polanya = r'\d\s*\d\s*\d'
+# cocok3 = re.findall(polanya, 'xx1 2   3xx')
+# cocok4 = re.findall(polanya, 'xx12 3xx')
+# cocok5 = re.findall(polanya, 'xx123xx')
+
+# # ^->cocok dengan awal string,jadi ini tidak akan menemukan:
+# cocok6 = re.findall(r'^m\w+', 'mejakursi')  # -> tidak ketemu, cocok==[]
+
+# # tapi tanpa ^ dia berhasil:
+# # => cocok == [’kursi tamu saya’]
+# cocok7 = re.findall(r'k[k\w\s]+', 'mejakursi tamu saya')
+
+# print(cocok1, cocok2, cocok3, cocok4, cocok5, cocok6, cocok7)
+
+
+# #========================Latihan 3=====================#
+# s = 'Alamatku adalah dita-b@google.com mas'
+# cocok6 = re.findall(r'\w+@\w+', s)
+# print(cocok6[0])
+
+# #========================Latihan 3.2=====================#
+# s = 'Alamatku adalah dita-b@google.com mas'
+# cocok = re.findall(r'[\w.-]+@[\w.-]+', s)
+# print(cocok[0])
+
+# cocok = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', s)
+# print(cocok[0])
+
+# ==========LATIHAN 4==========
+
+# s = 'Alamatku adalah dita-b@google.com mas'
+# cocok = re.findall(r'([\w.-]+)@([\w.-]+)', s)  # perhatikan posisi() dipolanya
+# cocok  # adalah [(’dita-b’, ’google.com’)]
+# # Bisa kita pilah satu persatu seperti ini:
+# print(cocok[0][0])  # 'dita-b'
+# print(cocok[0][1])  # 'google.com'
+# s = 'Alamatku sri@google.com serta joko@abc.com ok bro. atau don@email.com mungkin daffa12288@gmail.com atau daffa@mail.com'
+# # Di sini re.findall() mengembalikan list beranggotakan string alamat
+# pola = r'[\w\.-]+@[\w\.-]+'
+# e = re.findall(pola, s)
+# print(e)
+# # =>e akan berisi [’sri@google.com’, ’joko@abc.com’, ’don@email.com’]
+# pola2 = r'([\w\.-]+)@([\w\.-]+)'
+# e2 = re.findall(pola2, s)
+# print(e2)
+# # Atau kita cetak satu per satu:
+# for tup in e:
+#     print('user', tup[0], 'dengan host: ', tup[1])
+
+# ==========LATIHAN 7.5==========
+s = 'Alamatku sri@google.com serta joko@abc.com ok bro. atau don@email.com mungkin daffa12288@gmail.com atau daffa@mail.com bisa jadi otong@mail.com ditambah sayang@yahoo.com'
+
+pola = r'[\w\.-]+@[\w\.-]+'
+# di sini re.findall() mengembalikan list beranggotakan string alamat
+e = re.findall(pola, s)
+print(e)
+
+pola2 = r'([\w\.-]+)@([\w\.-]+)'
+e2 = re.findall(pola2, s)
+print(e2)
+
+for tup in e:
+    print('user', tup[0], 'dengan host: ', tup[1])
