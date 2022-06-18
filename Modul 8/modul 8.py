2c794c95765d6ec8a0a33337acf6356fb1987bc3
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2022-06-18 20:44:48 +0700

    tambahan

--- Modul 8/modul 8.py
+++ Modul 8/modul 8.py
@@ -1,56 +1,56 @@
-# class Stack:
-#     def __init__(self):
-#         self.items = []
+class Stack:
+    def __init__(self):
+        self.items = []
 
-#     def isEmpty(self):
-#         return len(self) == 0
+    def isEmpty(self):
+        return len(self) == 0
 
-#     def __len__(self):
-#         return len(self.items)
+    def __len__(self):
+        return len(self.items)
 
-#     def peek(self):
-#         assert not self.isEmpty()
-#         return self.items[-1]
+    def peek(self):
+        assert not self.isEmpty()
+        return self.items[-1]
 
-#     def pop(self):
-#         assert not self.isEmpty()
-#         return self.items.pop()
+    def pop(self):
+        assert not self.isEmpty()
+        return self.items.pop()
 
-#     def push(self, data):
-#         self.items.append(data)
-
-
-# class StackLL:
-#     def __init__(self):
-#         self.top = None
-#         self.size = 0
-
-#     def isEmpty(self):
-#         return self.top is None
-
-#     def __len__(self):
-#         return self.size
-
-#     def peek(self):
-#         assert not self.isEmpty()
-#         return self.top.item
-
-#     def pop(self):
-#         assert not self.isEmpty()
-#         node = self.top
-#         self.top = self.top.next
-#         self.size -= 1
-#         return node.item
-
-#     def push(self):
-#         self.top = _StackNode(data, self.top)
-#         self.size += 1
+    def push(self, data):
+        self.items.append(data)
 
 
-# class _StackNode:
-#     def __init__(self, data, link):
-#         self.item = data
-#         self.next = link
+class StackLL:
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def isEmpty(self):
+        return self.top is None
+
+    def __len__(self):
+        return self.size
+
+    def peek(self):
+        assert not self.isEmpty()
+        return self.top.item
+
+    def pop(self):
+        assert not self.isEmpty()
+        node = self.top
+        self.top = self.top.next
+        self.size -= 1
+        return node.item
+
+    def push(self):
+        self.top = _StackNode(data, self.top)
+        self.size += 1
+
+
+class _StackNode:
+    def __init__(self, data, link):
+        self.item = data
+        self.next = link
 
 
 # PROMPT = "Masukkan bilangan positif (<0 untuk mengakhiri) : "
@@ -64,22 +64,22 @@
 #     print(value)
 
 
-# def cetakBiner(d):
-#     f = Stack()
-#     if d == 0:
-#         f.push(0)
-#     while d != 0:
-#         sisa = d % 2
-#         d = d//2
-#         f.push(sisa)
-#     st = ""
-#     for i in range(len(f)):
-#         st = st + str(f.pop())
-#     return st
+def cetakBiner(d):
+    f = Stack()
+    if d == 0:
+        f.push(0)
+    while d != 0:
+        sisa = d % 2
+        d = d//2
+        f.push(sisa)
+    st = ""
+    for i in range(len(f)):
+        st = st + str(f.pop())
+    return st
 
 
-# print(cetakBiner(11))
-# print(cetakBiner(53))
+print(cetakBiner(11))
+print(cetakBiner(53))
 
 # class Queue(object):
 #     def __init__(self):
@@ -357,37 +357,37 @@
 
 
 # nomer 5
-class PriorityQueue(object):
-    def __init__(self):
-        self.qlist = []
+# class PriorityQueue(object):
+#     def __init__(self):
+#         self.qlist = []
 
-    def isEmpty(self):
-        return len(self) == 0
+#     def isEmpty(self):
+#         return len(self) == 0
 
-    def __len__(self):
-        return len(self.qlist)
+#     def __len__(self):
+#         return len(self.qlist)
 
-    def enqueue(self, data, priority):
-        entry = _PriorityQEntry(data, priority)
-        self.qlist.append(entry)
+#     def enqueue(self, data, priority):
+#         entry = _PriorityQEntry(data, priority)
+#         self.qlist.append(entry)
 
-    def dequeue(self):
-        assert not self.isEmpty(), "Antrian sedang kosong"
-        a = []
-        for i in self.qlist:
-            a.append(i.priority)
-        print(self.qlist.pop(a.index(min(a))).item)
-
-
-class _PriorityQEntry(object):
-    def __init__(self, data, priority):
-        self.item = data
-        self.priority = priority
+#     def dequeue(self):
+#         assert not self.isEmpty(), "Antrian sedang kosong"
+#         a = []
+#         for i in self.qlist:
+#             a.append(i.priority)
+#         print(self.qlist.pop(a.index(min(a))).item)
 
 
-S = PriorityQueue()
-S.enqueue("Jeruk", 4)
-S.enqueue("Tomat", 2)
-S.enqueue("Mangga", 0)
-S.enqueue("Duku", 4)
-S.enqueue("Pepaya", 2)
+# class _PriorityQEntry(object):
+#     def __init__(self, data, priority):
+#         self.item = data
+#         self.priority = priority
+
+
+# S = PriorityQueue()
+# S.enqueue("Jeruk", 4)
+# S.enqueue("Tomat", 2)
+# S.enqueue("Mangga", 0)
+# S.enqueue("Duku", 4)
+# S.enqueue("Pepaya", 2)
